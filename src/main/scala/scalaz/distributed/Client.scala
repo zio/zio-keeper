097eec144738c4ac868223b622318a7882b14f2e
Dejan Mijic <dmijic@acm.org>
2019-03-20 00:27:15 +0100

    Rename Module to Client and move factory to companion

--- src/main/scala/scalaz/distributed/Client.scala
+++ src/main/scala/scalaz/distributed/Client.scala
@@ -2,13 +2,23 @@ package scalaz.distributed
 
 import java.net.InetAddress
 
-trait Module {
+trait Client {
   final def key[K: Type, V: Type](k: K): Path[Map[K, V], V] = Path.Key[K, V](k)
 
   final def compose[A: Type, B: Type, C: Type](x: Path[A, B], y: Path[B, C]): Path[A, C] =
     Path.Composed(x, y)
 
-  def members(cb: Membership => F[Boolean]): F[Unit]
+  def connect(member: Member, seed: Set[InetAddress]): F[Protocol]
 
-  def startup(member: Member, seed: Set[InetAddress]): F[Protocol]
+  def members(cb: Membership => F[Boolean]): F[Unit]
+}
+
+object Client {
+
+  def default: Client =
+    new Client {
+      override def connect(member: Member, seed: Set[InetAddress]): F[Protocol] = ???
+
+      override def members(cb: Membership => F[Boolean]): F[Unit] = ???
+    }
 }
