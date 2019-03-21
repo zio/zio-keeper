bc5fdbafcc8af76613e738e0e2c04d208eba6a50
Dejan Mijic <dmijic@acm.org>
2019-03-21 23:37:47 +0100

    Define reified types

--- src/main/scala/scalaz/distributed/Reified.scala
+++ src/main/scala/scalaz/distributed/Reified.scala
@@ -3,5 +3,9 @@ package scalaz.distributed
 sealed trait Reified
 
 object Reified {
-  // TODO: type instances
+  case object Bool   extends Reified
+  case object Int    extends Reified
+  case object Long   extends Reified
+  case object Double extends Reified
+  case object String extends Reified
 }
--- src/main/scala/scalaz/distributed/Type.scala
+++ src/main/scala/scalaz/distributed/Type.scala
@@ -6,4 +6,15 @@ trait Type[A] {
 
 object Type {
   def apply[A](implicit instance: Type[A]): Type[A] = instance
+
+  implicit val bool: Type[scala.Boolean]         = instanceOf(Reified.Bool)
+  implicit val int: Type[scala.Int]              = instanceOf(Reified.Int)
+  implicit val long: Type[scala.Long]            = instanceOf(Reified.Long)
+  implicit val double: Type[scala.Double]        = instanceOf(Reified.Double)
+  implicit val string: Type[scala.Predef.String] = instanceOf(Reified.String)
+
+  private def instanceOf[A](r: Reified): Type[A] =
+    new Type[A] {
+      override def reified: Reified = r
+    }
 }
