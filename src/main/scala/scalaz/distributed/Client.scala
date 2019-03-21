922a617d8b06a4b595a9c5d73e9b835973afc695
Dejan Mijic <dmijic@acm.org>
2019-03-21 15:18:11 +0100

    Rename F to Distributed

--- src/main/scala/scalaz/distributed/Client.scala
+++ src/main/scala/scalaz/distributed/Client.scala
@@ -2,34 +2,38 @@ package scalaz.distributed
 
 import java.net.InetAddress
 
+import scalaz.zio.IO
+
 trait Client {
+  type Distributed[A] = IO[Error, A]
+
   type Type[A]
   type Path[A, B]
 
   trait Metadata {
-    def get[A: Type, B: Type](where: Path[A, B]): F[B]
-    def set[A: Type, B: Type](where: Path[A, B], b: B): F[Unit]
+    def get[A: Type, B: Type](where: Path[A, B]): Distributed[B]
+    def set[A: Type, B: Type](where: Path[A, B], b: B): Distributed[Unit]
 
-    final def modify[A: Type, B: Type](where: Path[A, B], f: B => B): F[Unit] =
+    final def modify[A: Type, B: Type](where: Path[A, B], f: B => B): Distributed[Unit] =
       for {
         c <- get(where)
         r <- set(where, f(c))
       } yield r
 
-    def increment[A: Type](where: Path[A, Int]): F[Unit]
+    def increment[A: Type](where: Path[A, Int]): Distributed[Unit]
   }
 
   trait Protocol {
-    def access[A: Type](id: MetadataID): F[Metadata]
+    def access[A: Type](id: MetadataID): Distributed[Metadata]
   }
 
   def key[K: Type, V: Type](k: K): Path[Map[K, V], V]
 
   def compose[A: Type, B: Type, C: Type](x: Path[A, B], y: Path[B, C]): Path[A, C]
 
-  def connect(member: Member, seed: Set[InetAddress]): F[Protocol]
+  def connect(member: Member, seed: Set[InetAddress]): Distributed[Protocol]
 
-  def members(cb: Membership => F[Boolean]): F[Unit]
+  def members(cb: Membership => Distributed[Boolean]): Distributed[Unit]
 }
 
 object Client {
@@ -44,8 +48,8 @@ object Client {
       override def compose[A: Type, B: Type, C: Type](x: Path[A, B], y: Path[B, C]): Path[A, C] =
         x >>> y
 
-      override def connect(member: Member, seed: Set[InetAddress]): F[Protocol] = ???
+      override def connect(member: Member, seed: Set[InetAddress]): Distributed[Protocol] = ???
 
-      override def members(cb: Membership => F[Boolean]): F[Unit] = ???
+      override def members(cb: Membership => Distributed[Boolean]): Distributed[Unit] = ???
     }
 }
--- src/main/scala/scalaz/distributed/package.scala
+++ src/main/scala/scalaz/distributed/package.scala
@@ -1,13 +1,10 @@
 package scalaz
 
-import scalaz.zio.IO
-
 package object distributed {
-  type F[A] = IO[Error, A]
+  type Distributed[A] = IO[Error, A]
 
   implicit class PathSyntax[A: SupportedType, B: SupportedType](self: PathElem[A, B]) {
     final def >>> [C: SupportedType](that: PathElem[B, C]): PathElem[A, C] =
       PathElem.Composed(self, that)
   }
-
 }
