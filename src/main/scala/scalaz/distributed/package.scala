d06cd80f3124f926aafba3df09049d201f81b0f1
Dejan Mijic <dmijic@acm.org>
2019-03-21 14:56:23 +0100

    Move PathSyntax to package object

--- src/main/scala/scalaz/distributed/package.scala
+++ src/main/scala/scalaz/distributed/package.scala
@@ -4,4 +4,10 @@ import scalaz.zio.IO
 
 package object distributed {
   type F[A] = IO[Error, A]
+
+  implicit class PathSyntax[A: SupportedType, B: SupportedType](self: PathElem[A, B]) {
+    final def >>> [C: SupportedType](that: PathElem[B, C]): PathElem[A, C] =
+      PathElem.Composed(self, that)
+  }
+
 }
