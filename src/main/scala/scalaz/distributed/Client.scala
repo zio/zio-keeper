742b4f5e747bb2ca05ca2c74db9896af00e3937d
Dejan Mijic <dmijic@acm.org>
2019-03-21 15:21:27 +0100

    Remove modify and increment

--- src/main/scala/scalaz/distributed/Client.scala
+++ src/main/scala/scalaz/distributed/Client.scala
@@ -5,22 +5,12 @@ import java.net.InetAddress
 import scalaz.zio.IO
 
 trait Client {
-  type Distributed[A] = IO[Error, A]
-
   type Type[A]
   type Path[A, B]
 
   trait Metadata {
     def get[A: Type, B: Type](where: Path[A, B]): Distributed[B]
     def set[A: Type, B: Type](where: Path[A, B], b: B): Distributed[Unit]
-
-    final def modify[A: Type, B: Type](where: Path[A, B], f: B => B): Distributed[Unit] =
-      for {
-        c <- get(where)
-        r <- set(where, f(c))
-      } yield r
-
-    def increment[A: Type](where: Path[A, Int]): Distributed[Unit]
   }
 
   trait Protocol {
