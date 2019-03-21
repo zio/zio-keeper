1022ee3693496408f6ce506387fba91f01cf0ab4
Dejan Mijic <dmijic@acm.org>
2019-03-21 23:06:33 +0100

    Expose default implementation through package object

--- src/main/scala/scalaz/distributed/Client.scala
+++ src/main/scala/scalaz/distributed/Client.scala
@@ -2,7 +2,11 @@ package scalaz.distributed
 
 import java.net.InetAddress
 
+import scalaz.zio.IO
+
 trait Client {
+  type Distributed[A] = IO[Error, A]
+
   type Type[A]
 
   trait Metadata {
@@ -21,21 +25,4 @@ trait Client {
   def connect(member: Member, seed: Set[InetAddress]): Distributed[Protocol]
 
   def members(cb: Membership => Distributed[Boolean]): Distributed[Unit]
-}
-
-object Client {
-
-  def default: Client =
-    new Client {
-      type Type[A] = SupportedType[A]
-
-      override def key[K: Type, V: Type](k: K): Path[Map[K, V], V] = Path.Key[K, V](k)
-
-      override def compose[A: Type, B: Type, C: Type](x: Path[A, B], y: Path[B, C]): Path[A, C] =
-        x >>> y
-
-      override def connect(member: Member, seed: Set[InetAddress]): Distributed[Protocol] = ???
-
-      override def members(cb: Membership => Distributed[Boolean]): Distributed[Unit] = ???
-    }
 }
--- src/main/scala/scalaz/distributed/package.scala
+++ src/main/scala/scalaz/distributed/package.scala
@@ -1,8 +1,17 @@
 package scalaz
 
-import scalaz.zio.IO
-
 package object distributed {
-  type Distributed[A] = IO[Error, A]
 
+  val client = new Client {
+    type Type[A] = SupportedType[A]
+
+    override def key[K: Type, V: Type](k: K): Path[Map[K, V], V] = Path.Key[K, V](k)
+
+    override def compose[A: Type, B: Type, C: Type](x: Path[A, B], y: Path[B, C]): Path[A, C] =
+      x >>> y
+
+    override def connect(member: Member, seed: Set[InetAddress]): Distributed[Protocol] = ???
+
+    override def members(cb: Membership => Distributed[Boolean]): Distributed[Unit] = ???
+  }
 }
