8dbb42dd46b8bcff09a890bc4c4ce477828f25b4
Dejan Mijic <dmijic@acm.org>
2019-06-29 17:37:51 +0200

    Remove undefined artifacts

--- src/main/scala/zio/keeper/Cluster.scala
+++ src/main/scala/zio/keeper/Cluster.scala
@@ -7,18 +7,10 @@ import zio.stream.Stream
 import zio.keeper.Cluster._
 
 trait Cluster {
-  final def join: ZIO[Credentials with Discovery with Transport with Metadata, Error, Handle] = ???
+  final def join: ZIO[Credentials with Discovery with Transport, Error, Unit] = ???
 }
 
 object Cluster {
-  // TODO: properly represent (see current 'develop' branch)
-  type Path[_, _]
-  type Type[_]
-
-  object Type {
-    def apply[A](implicit instance: Type[A]): Type[A] = instance
-  }
-
   trait Credentials {
     // TODO: ways to obtain auth data
   }
@@ -34,13 +26,5 @@ object Cluster {
   trait Transport {
     def send[S, A](data: Message[S, A]): IO[Error, Unit]
     def receive[S, A]: Stream[Error, Message[S, A]]
-  }
-
-  // TODO: maybe just an enumeration specifying the type of underlying data structure, e.g. CRDT
-  trait Metadata {}
-
-  trait Handle {
-    def get[A: Type, B: Type](where: Path[A, B]): IO[Error, B]
-    def set[A: Type, B: Type](where: Path[A, B], b: B): IO[Error, Unit]
   }
 }
