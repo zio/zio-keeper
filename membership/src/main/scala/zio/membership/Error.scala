d6e6df5b37e39b83b460ecf90b28e9a3bbebfb35
Maxim Schuwalow <maxim.schuwalow@zalando.de>
2019-11-26 04:02:51 +0100

    add maintainer

--- membership/src/main/scala/zio/membership/Error.scala
+++ membership/src/main/scala/zio/membership/Error.scala
@@ -6,14 +6,18 @@ import zio.nio.SocketAddress
 sealed abstract class Error(msg: String = "", cause: Throwable = null) extends Exception(msg, cause)
 
 sealed abstract class SendError(msg: String = "", cause: Throwable = null) extends Error(msg, cause)
+
 object SendError {
   final case class ExceptionThrown(exc: Throwable) extends SendError(cause = exc)
-  final case class Timeout(timeout: Duration) extends SendError(msg = s"Violated timeout $timeout.")
+  final case class Timeout(timeout: Duration)      extends SendError(msg = s"Violated timeout $timeout.")
 }
 
 sealed abstract class ReceiveError(msg: String = "", cause: Throwable = null) extends Error(msg, cause)
+
 object ReceiveError {
-  final case class BindFailed(addr: SocketAddress, exc: Throwable) extends ReceiveError(msg = s"Failed binding to address $addr.", cause = exc)
+
+  final case class BindFailed(addr: SocketAddress, exc: Throwable)
+      extends ReceiveError(msg = s"Failed binding to address $addr.", cause = exc)
   final case class ExceptionThrown(exc: Throwable) extends ReceiveError(cause = exc)
 }
 
--- membership/src/test/scala/zio/membership/transport/TcpTransportSpec.scala
+++ membership/src/test/scala/zio/membership/transport/TcpTransportSpec.scala
@@ -12,39 +12,40 @@ import zio.test.environment.Live
 import zio.test.environment.TestEnvironment
 import zio.test.TestAspect._
 
-object TransportSpec extends DefaultRunnableSpec({
-  // todo: actually find free port
-  def freePort = ZIO.succeed(8010)
+object TransportSpec
+    extends DefaultRunnableSpec({
+      // todo: actually find free port
+      def freePort = ZIO.succeed(8010)
 
-  val connectDelay = 300.milliseconds
+      val connectDelay = 300.milliseconds
 
-  val withTransport = tcp.withTcpTransport(10, 10.seconds, 10.seconds)
+      val withTransport = tcp.withTcpTransport(10, 10.seconds, 10.seconds)
 
-  def environment: ZIO[TestEnvironment, Nothing, TestClock with Transport with Live[Clock with Transport]] =
-    for {
-      clock <- Live.live(ZIO.environment[Clock])
-      tClock <- ZIO.environment[TestClock]
-      live <- (ZIO.succeed(clock) @@ withTransport).flatMap(Live.make(_))
-      test <- (ZIO.succeed(tClock) @@ withTransport)
-      env <- ZIO.succeed(live) @@ enrichWith(test)
-    } yield env
+      def environment: ZIO[TestEnvironment, Nothing, TestClock with Transport with Live[Clock with Transport]] =
+        for {
+          clock  <- Live.live(ZIO.environment[Clock])
+          tClock <- ZIO.environment[TestClock]
+          live   <- (ZIO.succeed(clock) @@ withTransport).flatMap(Live.make(_))
+          test   <- (ZIO.succeed(tClock) @@ withTransport)
+          env    <- ZIO.succeed(live) @@ enrichWith(test)
+        } yield env
 
-  suite("TcpTransport")(
-    testM("can send and receive messages") {
-      checkM(Gen.listOf(Gen.anyByte)) { bytes =>
-        val payload = Chunk.fromIterable(bytes)
+      suite("TcpTransport")(
+        testM("can send and receive messages") {
+          checkM(Gen.listOf(Gen.anyByte)) { bytes =>
+            val payload = Chunk.fromIterable(bytes)
 
-        def readOne(addr: SocketAddress) =
-          bind(addr).take(1).runCollect.map(_.head)
+            def readOne(addr: SocketAddress) =
+              bind(addr).take(1).runCollect.map(_.head)
 
-        environment >>> Live.live(for {
-          port <- freePort
-          addr <- SocketAddress.inetSocketAddress(port)
-          chunk <- readOne(addr).fork
-          _    <- send(addr, payload).delay(connectDelay)
-          result <- chunk.join
-        } yield assert(result, equalTo(payload)))
-      }
-    } @@ flaky @@ timeout(120.seconds) // this one is slow
-  )
-})
+            environment >>> Live.live(for {
+              port   <- freePort
+              addr   <- SocketAddress.inetSocketAddress(port)
+              chunk  <- readOne(addr).fork
+              _      <- send(addr, payload).delay(connectDelay)
+              result <- chunk.join
+            } yield assert(result, equalTo(payload)))
+          }
+        } @@ flaky @@ timeout(120.seconds) // this one is slow
+      )
+    })
