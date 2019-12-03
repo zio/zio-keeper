1a0dd1e369237206e11865eb56ef609b1555c43d
Przemyslaw Wierzbicki2 <dev.Wierzbicki2.Przemyslaw@dev.bwater.com>
2019-12-03 18:13:53 -0500

    cluster tests

--- keeper/src/test/scala/zio/keeper/ClusterSpec.scala
+++ keeper/src/test/scala/zio/keeper/ClusterSpec.scala
@@ -1,13 +1,16 @@
 package zio.keeper
 
+import zio.clock.Clock
+import zio.console.Console
 import zio.duration._
 import zio.keeper.Cluster.Credentials
 import zio.keeper.discovery.Discovery
 import zio.keeper.transport.tcp
 import zio.macros.delegate.{enrichWith, _}
 import zio.nio.SocketAddress
+import zio.random.Random
 import zio.test.Assertion._
-import zio.test.environment.{TestClock, TestConsole, TestRandom}
+import zio.test.environment.{Live, TestClock, TestConsole, TestRandom}
 import zio.test.{DefaultRunnableSpec, _}
 import zio.{IO, Promise, Ref, ZIO}
 
@@ -20,15 +23,15 @@ object ClusterSpec extends DefaultRunnableSpec({
       transport <- (ZIO.environment[TestClock with TestConsole with TestRandom] @@ withTransport)
       config: Credentials with Discovery = new Credentials with Discovery {
         override val discover: IO[Error, Set[SocketAddress]] =
-          for {
-            members <- clusterMembers.get
-            addrs <- ZIO.collectAll(members.map(_.addr.socketAddress))
-          } yield addrs.toSet
+        for {
+          members <- clusterMembers.get
+          addrs <- ZIO.collectAll(members.map(_.addr.socketAddress))
+        } yield addrs.toSet
       }
       result <- ZIO.succeed(config) @@ enrichWith(transport)
-//      live <- (Live.live(ZIO.environment[Clock with Console with Discovery with Credentials]) @@ enrichWith(result)).flatMap(Live.make)
-//      env  <- ZIO.succeed(result) @@ enrichWith(result)
-    } yield result
+      result <- (Live.live(ZIO.environment[Console with Clock with Random]) @@ enrichWith(result)).flatMap(Live.make)
+      env  <- ZIO.succeed(result) @@ enrichWith(config)
+    } yield env
 
   def createCluster = Cluster
     .join(0)
@@ -36,14 +39,16 @@ object ClusterSpec extends DefaultRunnableSpec({
 
   testM("blah"){
     Ref.make(Set.empty[Member]).flatMap(membersRef =>
-      environment(membersRef) >>>
-        (for {
-          shutdown <- Promise.make[Nothing, Unit]
-          _ <- createCluster.use(c => membersRef.update(_ + c.localMember) *> shutdown.await)
-          _ <- shutdown.succeed(())
-        } yield assert("ZIO.unit", equalTo("a")))
+      environment(membersRef) >>> Live.live(
+                (for {
+                  shutdown <- Promise.make[Nothing, Unit]
+                  _ <- createCluster.use(c => membersRef.update(_ + c.localMember) *> shutdown.await)
+                  _ <- shutdown.succeed(())
+                } yield assert("ZIO.unit", equalTo("a")))
+      ))
 
-    )
+
+    
 
 //    assertM(ZIO.unit, equalTo("ss"))
   }
