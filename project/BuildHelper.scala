d5aaa65013b2b7139009c1d55fc7b53b57c6c021
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-03-14 21:14:00 +0100

    Update zio-nio to 1.0.0-RC5 (#194)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -15,7 +15,7 @@ object BuildHelper {
   final private val Scala213 = "2.13.1"
 
   final val ZioVersion        = "1.0.0-RC18-2"
-  final val NioVersion        = "1.0.0-RC4"
+  final val NioVersion        = "1.0.0-RC5"
   final val ZioLoggingVersion = "0.2.4"
 
   final private val CommonOpts =
