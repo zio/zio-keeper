0d8df959061c6a058f0a8a02cf116be580c273f4
Maxim Schuwalow <maxim.schuwalow@zalando.de>
2020-05-27 00:32:37 +0200

    followup

--- keeper/src/main/scala/zio/keeper/membership/hyparview/Round.scala
+++ keeper/src/main/scala/zio/keeper/membership/hyparview/Round.scala
@@ -2,6 +2,7 @@ package zio.keeper.membership.hyparview
 
 import zio._
 import zio.keeper.ByteCodec
+import zio.keeper.encoding._
 import zio.keeper.SerializationError.DeserializationTypeError
 
 sealed abstract case class Round(value: Int) {
@@ -15,12 +16,12 @@ object Round {
   implicit val codec: ByteCodec[Round] =
     ByteCodec.instance(
       chunk =>
-        ZIO.effect(BigInt(chunk.toArray).intValue).mapError(DeserializationTypeError(_)).flatMap {
+        byteArrayToInt(chunk.toArray).flatMap {
           case x if x >= 0 => ZIO.succeed(new Round(x) {})
           case x           => ZIO.fail(DeserializationTypeError(s"Invalid range for round $x"))
         }
     )(
-      round => ZIO.succeed(Chunk.fromArray(BigInt(round.value).toByteArray))
+      round => ZIO.succeed(Chunk.fromArray(intToByteArray(round.value)))
     )
 
   implicit val ordering: Ordering[Round] =
