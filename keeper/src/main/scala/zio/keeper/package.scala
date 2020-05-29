3f3ae4b38254861b74356d4735b5889df29a5b72
Dejan Mijic <dmijic@acm.org>
2020-05-30 00:28:14 +0200

    Split into membership and consensus modules

--- keeper/src/main/scala/zio/keeper/package.scala
+++ keeper/src/main/scala/zio/keeper/package.scala
@@ -2,21 +2,8 @@ package zio
 
 import zio.stream._
 
-package object keeper {
-  type Keeper[A] = Has[Keeper.Service[A]]
-
-  object Keeper {
-
-    trait Service[A] {
-      def broadcast(data: A): UIO[Unit]
-      def send(data: A, recepient: NodeAddress): UIO[Unit]
-      def receive: Stream[Nothing, (NodeAddress, A)]
-
-      // TODO: add consensus stuff
-    }
-
-    // TODO: provide live part
-  }
+package object keeper extends MembershipProtocol with ConsensusProtocol {
+  type Keeper[A] = Membership[A] with Consensus
 
   def broadcast[A](data: A): ZIO[Keeper[A], Nothing, Unit] = ???
 
