939fda3d453bf1f523a7b0bf00637e794cc6fad8
Maxim Schuwalow <maxim.schuwalow@zalando.de>
2020-08-04 21:00:05 +0200

    add error handling combinators to Connection

--- keeper/src/main/scala/zio/keeper/transport/Connection.scala
+++ keeper/src/main/scala/zio/keeper/transport/Connection.scala
@@ -61,6 +61,20 @@ trait Connection[-R, +E, -I, +O] { self =>
 
     }
 
+  def mapError[E1](f: E => E1): Connection[R, E1, I, O] =
+    new Connection[R, E1, I, O] {
+
+      def send(data: I): ZIO[R, E1, Unit] =
+        self.send(data).mapError(f)
+
+      val receive: ZStream[R, E1, O] =
+        self.receive.mapError(f)
+
+      val close: zio.UIO[Unit] =
+        self.close
+
+    }
+
   def mapM[R1 <: R, E1 >: E, O1](f: O => ZIO[R1, E1, O1]): Connection[R1, E1, I, O1] =
     new Connection[R1, E1, I, O1] {
 
@@ -71,6 +85,29 @@ trait Connection[-R, +E, -I, +O] { self =>
         self.receive.mapM(f)
 
       val close: UIO[Unit] =
+        self.close
+
+    }
+
+  def recoverErrors[R1 <: R, E1, I1 <: I, O1 >: O](
+    f: (E, I1) => ZIO[R1, E1, Unit],
+    g: E => ZIO[R1, E1, O1]
+  ): Connection[R1, E1, I1, O1] =
+    new Connection[R1, E1, I1, O1] {
+
+      def send(data: I1): ZIO[R1, E1, Unit] =
+        self.send(data).foldM(f(_, data), ZIO.succeedNow)
+
+      // TODO: this is a hack. Switch to proper error handling combinators once ZStream supports them.
+      val receive: ZStream[R1, E1, O1] = {
+        def go(stream: ZStream[R, E, O]): ZStream[R1, E1, O1] =
+          stream.catchAll { e =>
+            ZStream.fromEffect(g(e)) ++ go(stream)
+          }
+        go(self.receive)
+      }
+
+      val close: zio.UIO[Unit] =
         self.close
 
     }
