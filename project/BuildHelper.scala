5f9e3018b5120d999c1d04fe9428bad97a5d1c2c
Scala Steward <me@scala-steward.org>
2020-09-13 11:20:38 +0200

    Update zio-config to 1.0.0-RC27

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -17,7 +17,7 @@ object BuildHelper {
   final val ZioVersion        = "1.0.0-RC21-2"
   final val NioVersion        = "1.0.0-RC8"
   final val ZioLoggingVersion = "0.3.2"
-  final val ZioConfigVersion  = "1.0.0-RC25"
+  final val ZioConfigVersion  = "1.0.0-RC27"
 
   final private val CommonOpts =
     Seq(
